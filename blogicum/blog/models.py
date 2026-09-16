dfc2c11e206e003e98d1efd8de8f107aef6c3aff
Ivan-alt2 <2011bor6@gmail.com>
2026-09-16 15:47:52 +0900

    Update models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,17 +1,16 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
 class TimestampModel(models.Model):
-    """Абстрактная модель для отслеживания времени создания и статуса публикации."""
-
     is_published = models.BooleanField(
         'Опубликовано',
         default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
     created_at = models.DateTimeField('Добавлено', auto_now_add=True)
 
     class Meta:
@@ -19,19 +18,17 @@ class TimestampModel(models.Model):
 
 
 class Category(TimestampModel):
-    """Категория."""
-
     title = models.CharField('Заголовок', max_length=256)
     description = models.TextField('Описание')
     slug = models.SlugField(
-    'Идентификатор',
-    max_length=64,
-    unique=True,
-    help_text=(
-        'Идентификатор страницы для URL; разрешены символы '
-        'латиницы, цифры, дефис и подчёркивание.'
+        'Идентификатор',
+        max_length=64,
+        unique=True,
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы '
+            'латиницы, цифры, дефис и подчёркивание.'
+        )
     )
-)
 
     class Meta:
         verbose_name = 'категория'
@@ -42,8 +39,6 @@ class Category(TimestampModel):
 
 
 class Location(TimestampModel):
-    """Местоположение."""
-
     name = models.CharField('Название места', max_length=256)
 
     class Meta:
@@ -51,13 +46,10 @@ class Location(TimestampModel):
         verbose_name_plural = 'Местоположения'
 
     def __str__(self):
-        # Оставлено только название без служебной информации
         return self.name
 
 
 class Post(TimestampModel):
-    """Публикация."""
-
     title = models.CharField('Заголовок', max_length=256)
     text = models.TextField('Текст')
     pub_date = models.DateTimeField(
@@ -70,7 +62,7 @@ class Post(TimestampModel):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts',  # Множественное число
+        related_name='posts',
         verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
@@ -78,14 +70,14 @@ class Post(TimestampModel):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts',  # Множественное число
+        related_name='posts',
         verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='posts',  # Множественное число
+        related_name='posts',
         verbose_name='Категория'
     )
 
